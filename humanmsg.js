5effa76df041ead8b1c5f29a9a5984bad52b85f4
Logan Koester <logan@logankoester.com>
2010-08-12 00:13:43 -0400

    Optional timeout and logging, new defaults

--- humanmsg.js
+++ humanmsg.js
@@ -5,17 +5,23 @@
 */
 
 var humanMsg = {
-	setup: function(appendTo, logName, msgOpacity) {
+	setup: function(appendTo, logName, msgOpacity, timeout) {
 		humanMsg.msgID = 'humanMsg';
 		humanMsg.logID = 'humanMsgLog';
+    humanMsg.logging = true;
+
+    if (timeout != undefined)
+      humanMsg.timeout = timeout;
 
 		// appendTo is the element the msg is appended to
 		if (appendTo == undefined)
 			appendTo = 'body';
 
 		// The text on the Log tab
-		if (logName == undefined)
+		if (logName == undefined) {
 			logName = 'Message Log';
+      humanMsg.logging = false; // Disable the message log
+    }
 
 		// Opacity of the message
 		humanMsg.msgOpacity = .8;
@@ -23,12 +29,13 @@ var humanMsg = {
 		if (msgOpacity != undefined) 
 			humanMsg.msgOpacity = parseFloat(msgOpacity);
 
+
 		// Inject the message structure
 		jQuery(appendTo).append('<div id="'+humanMsg.msgID+'" class="humanMsg"><div class="round"></div><p></p><div class="round"></div></div> <div id="'+humanMsg.logID+'"><p>'+logName+'</p><ul></ul></div>')
 		
-		jQuery('#'+humanMsg.logID+' p').click(
-			function() { jQuery(this).siblings('ul').slideToggle() }
-		)
+    jQuery('#'+humanMsg.logID+' p').click(
+      function() { jQuery(this).siblings('ul').slideToggle() }
+    )
 	},
 
 	displayMsg: function(msg) {
@@ -42,22 +49,27 @@ var humanMsg = {
 	
 		// Show message
 		jQuery('#'+humanMsg.msgID+'').show().animate({ opacity: humanMsg.msgOpacity}, 200, function() {
-			jQuery('#'+humanMsg.logID)
-				.show().children('ul').prepend('<li>'+msg+'</li>')	// Prepend message to log
-				.children('li:first').slideDown(200)				// Slide it down
+      if (humanMsg.logging) {
+        jQuery('#'+humanMsg.logID)
+          .show().children('ul').prepend('<li>'+msg+'</li>')	// Prepend message to log
+          .children('li:first').slideDown(200)				// Slide it down
 		
-			if ( jQuery('#'+humanMsg.logID+' ul').css('display') == 'none') {
-				jQuery('#'+humanMsg.logID+' p').animate({ bottom: 40 }, 200, 'linear', function() {
-					jQuery(this).animate({ bottom: 0 }, 300, 'easeOutBounce', function() { jQuery(this).css({ bottom: 0 }) })
-				})
-			}
+        if ( jQuery('#'+humanMsg.logID+' ul').css('display') == 'none') {
+          jQuery('#'+humanMsg.logID+' p').animate({ bottom: 40 }, 200, 'linear', function() {
+            jQuery(this).animate({ bottom: 0 }, 300, 'easeOutBounce', function() { jQuery(this).css({ bottom: 0 }) })
+          })
+        }
+      }
 			
 		})
 
 		// Watch for mouse & keyboard in .5s
 		humanMsg.t1 = setTimeout("humanMsg.bindEvents()", 700)
-		// Remove message after 5s
-		humanMsg.t2 = setTimeout("humanMsg.removeMsg()", 5000)
+
+    if (humanMsg.timeout) {
+      // Remove message after 5s
+      humanMsg.t2 = setTimeout("humanMsg.removeMsg()", humanMsg.timeout)
+    }
 	},
 
 	bindEvents: function() {
